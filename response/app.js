9bf151305ec545183a5805ef3e9220d1f2b974e5
Ace Brown <nanantim888@gmail.com>
2020-11-23 11:59:32 +0000

    Create app.js

---
+++ response/app.js
@@ -0,0 +1,12 @@
+function Game() {
+    document.write('Hello Friend!');
+}
+
+Game();
+
+var msg = 'Sign up to receive our newsletter for 10% off!';
+function updateMessage() {
+var el = document.getElementByld('message');
+el .textContent = msg;
+}
+updateMessage(); 
